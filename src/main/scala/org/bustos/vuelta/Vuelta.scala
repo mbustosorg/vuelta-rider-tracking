ae949f81c3b555d211cc95889ee5f175963b546a
Mauricio Bustos <mauricio@monkeypro.gateway.pace.com>
2015-10-26 12:28:27 -0700

    argument handling

--- src/main/scala/org/bustos/vuelta/Vuelta.scala
+++ src/main/scala/org/bustos/vuelta/Vuelta.scala
@@ -4,10 +4,9 @@ import akka.actor.{ActorSystem, Props}
 import akka.io.IO
 import akka.pattern.ask
 import akka.util.Timeout
-import scala.util.{Failure, Success}
 import spray.can.Http
 import com.typesafe.config.ConfigFactory
-
+import scala.util.Properties.envOrElse
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -21,8 +20,10 @@ object Vuelta extends App {
     val server = system.actorOf(Props[VueltaServiceActor], "vueltaRoutes")
     val config = ConfigFactory.load
 
+    val port = envOrElse("PORT", config.getString("vueltaServer.port"))
+
     if (args.length > 0) IO(Http) ? Http.Bind(server, "0.0.0.0", args(0).toInt)
-    else IO(Http) ? Http.Bind(server, "0.0.0.0",  config.getInt("vueltaServer.port"))
+    else IO(Http) ? Http.Bind(server, "0.0.0.0", port.toInt)
   }
 
   doMain()
