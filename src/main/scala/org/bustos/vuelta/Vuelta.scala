3b490a453cb25a4dd11bbea977cdb4727072ab3b
Mauricio Bustos <mauricio@monkeyPro.local>
2015-11-09 13:29:53 -0800

    license updates

--- src/main/scala/org/bustos/vuelta/Vuelta.scala
+++ src/main/scala/org/bustos/vuelta/Vuelta.scala
@@ -1,3 +1,22 @@
+/*
+
+    Copyright (C) 2015 Mauricio Bustos (m@bustos.org)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 package org.bustos.vuelta
 
 import java.io.File
@@ -38,7 +57,7 @@ object Vuelta extends App {
     db.withSession { implicit session =>
       riderTable.filter(_.bibNumber > 0).delete
       riderEventTable.filter(_.bibNumber > 0).delete
-      val reader = CSVReader.open(new File("/Users/mauricio/Downloads/vueltaRiders - riders.csv"))
+      val reader = CSVReader.open(new File("/Users/mauricio/Downloads/Rider Tracking - Numeric.csv"))
       reader.foreach(fields => {
         println(fields)
         if (fields(1) != "" && fields(1).forall(_.isDigit)) {
